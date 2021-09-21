4d492efa9af4e6b024e872f710750f364673743d
Suanya <sonjaunger@eyewant.me>
2021-09-21 11:26:26 -0700

    EndOfPart9
    Geometries

--- 09_Geometries/Exercise/src/script.js
+++ 09_Geometries/Exercise/src/script.js
@@ -12,8 +12,54 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const geometry = new THREE.BoxBufferGeometry(1, 1, 1, 2, 2, 2)
+
+// Let's do the Triangle again. From the top!
+const geometry = new THREE.BufferGeometry()
+
+const count = 50
+const positionsArray = new Float32Array(count * 3 * 3)
+
+for(let i = 0; i < count * 3 * 3; i++)
+{
+    positionsArray[i] = (Math.random() - 0.5) * 6
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute('position', positionsAttribute)
+
+/* TheFirstTriangle
+const positionsArray = new Float32Array( // let's fill this arry with values
+    [
+        0,0,0,
+        0,1,0,
+        1,0,0
+    ])    
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+const geometry = new THREE.BufferGeometry()
+geometry.setAttribute('position', positionsAttribute)
+/*/
+
+/* TheLongWay
+const positionsArray = new Float32Array(9)
+positionsArray[0] = 0
+positionsArray[1] = 0
+positionsArray[2] = 0
+
+positionsArray[3] = 0
+positionsArray[4] = 1
+positionsArray[5] = 0
+
+positionsArray[6] = 1
+positionsArray[7] = 0
+positionsArray[8] = 0
+*/
+
+const material = new THREE.MeshBasicMaterial(
+    { 
+    color: 0xffff00, 
+    wireframe: true
+    })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
