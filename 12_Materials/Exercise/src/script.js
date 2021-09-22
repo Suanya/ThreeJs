2ed58e1c354b820e51b6d8239aa53932232b9858
Suanya <sonjaunger@eyewant.me>
2021-09-22 07:06:17 -0700

    EndOfLesson12

--- 12_Materials/Exercise/src/script.js
+++ 12_Materials/Exercise/src/script.js
@@ -11,91 +11,77 @@ const gui = new dat.GUI()
 /**
  * Textures
  */
+const textureLoader = new THREE.TextureLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
 
-const TextureLoader = new THREE.TextureLoader()
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
+const gradientTexture = textureLoader.load('/textures/gradients/3.png')
 
-const doorColorTexture = TextureLoader.load('/textures/door/color.jpg')
-const doorAlphaTexture = TextureLoader.load('/textures/door/alpha.jpg')
-const doorAmbientOcclusionTexture = TextureLoader.load('/textures/door/ambientOcclusion.jpg')
-const doorHeightTexture = TextureLoader.load('/textures/door/height.jpg')
-const doorNormalTexture = TextureLoader.load('/textures/door/normal.jpg')
-const doorMetalnessTexture = TextureLoader.load('/textures/door/metalness.jpg')
-const doorRoughnessTexture = TextureLoader.load('/textures/door/roughness.jpg')
-
-const matcapTexture = TextureLoader.load('/textures/matcaps/8.png')
-const gradientTexture = TextureLoader.load('/textures/gradients/3.png')
 gradientTexture.minFilter = THREE.NearestFilter
 gradientTexture.magFilter = THREE.NearestFilter
 gradientTexture.generateMipmaps = false
 
-
+const environmentMapTexture = cubeTextureLoader.load( // images must be in the right order like in folder!!!
+    [
+        '/textures/environmentMaps/4/px.png',
+        '/textures/environmentMaps/4/nx.png',
+        '/textures/environmentMaps/4/py.png',
+        '/textures/environmentMaps/4/ny.png',
+        '/textures/environmentMaps/4/pz.png',
+        '/textures/environmentMaps/4/ny.png'
+    ]
+)
 
 /**
  * Base
  */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
-
 // Scene
 const scene = new THREE.Scene()
 
 /**
  * Objects
  */
-/*
-const material = new THREE.MeshBasicMaterial()
-material.map = doorColorTexture
-material.color = new THREE.Color(0xff0ff0)
-material.wireframe = true
-material.opacity = 0.5
-material.transparent = true
-material.alphaMap = doorAlphaTexture
-material.side = THREE.BackSide
-material.side = THREE.DoubleSide 
-material.wireframe = true
 
-const material = new THREE.MeshNormalMaterial()
-material.flatShading = true
-
-const material = new THREE.MeshMatcapMaterial()
-material.matcap = matcapTexture
-
-const material = new THREE.MeshDepthMaterial()
-
-const material = new THREE.MeshLambertMaterial()
-
-const material = new THREE.MeshPhongMaterial()
-material.shininess = 50
-material.specular = new THREE.Color(0x1188ff)
-
-const material = new THREE.MeshToonMaterial()
-material.gradientMap = gradientTexture
-*/
-
+// Environment Maps
 const material = new THREE.MeshStandardMaterial()
-material.metalness = 0.45
-material.roughness = 0.65
+material.metalness = 0.7
+material.roughness = 0.2
+material.envMap = environmentMapTexture
+
 
 gui.add(material, 'metalness').min(0).max(1).step(0.0001)
 gui.add(material, 'roughness').min(0).max(1).step(0.0001)
-
+gui.add(material, 'aoMapIntensity').min(0).max(10).step(0.0001)
+gui.add(material, 'displacementScale').min(0).max(10).step(0.0001)
 
 const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.SphereGeometry(0.5, 64, 64),
     material
 )
 sphere.position.x = -1.5
+sphere.geometry.setAttribute('uv2', new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2))
 
 const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(1, 1),
+    new THREE.PlaneGeometry(1, 1, 100, 100),
     material
 )
+plane.geometry.setAttribute('uv2', new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2))
 
 const torus = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+    new THREE.TorusGeometry(0.3, 0.2, 64, 128),
     material
 )
 torus.position.x = 1.5
+torus.geometry.setAttribute('uv2', new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2))
 
 scene.add(sphere, plane, torus)
 
@@ -110,6 +96,7 @@ pointLight.position.x = 2
 pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight) 
+
 
 
 /**
@@ -188,3 +175,66 @@ const tick = () =>
 }
 
 tick()
+
+
+
+
+
+
+
+
+// AddOns
+
+/**
+ * Objects
+ */
+/*
+const material = new THREE.MeshBasicMaterial()
+material.map = doorColorTexture
+material.color = new THREE.Color(0xff0ff0)
+material.wireframe = true
+material.opacity = 0.5
+material.transparent = true
+material.alphaMap = doorAlphaTexture
+material.side = THREE.BackSide
+material.side = THREE.DoubleSide 
+material.wireframe = true
+
+const material = new THREE.MeshNormalMaterial()
+material.flatShading = true
+
+const material = new THREE.MeshMatcapMaterial()
+material.matcap = matcapTexture
+
+const material = new THREE.MeshDepthMaterial()
+
+const material = new THREE.MeshLambertMaterial()
+
+const material = new THREE.MeshPhongMaterial()
+material.shininess = 50
+material.specular = new THREE.Color(0x1188ff)
+
+const material = new THREE.MeshToonMaterial() // doesn't work
+material.gradientMap = gradientTexture // cartoon is gone
+
+const material = new THREE.MeshToonMaterial() // doesn't work
+material.gradientMap = gradientTexture // cartoon is gone
+
+*/
+
+/* DOOR
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.45
+material.roughness = 0.65
+material.map = doorColorTexture
+material.aoMap = doorAmbientOcclusionTexture
+material.aoMapIntensity = 1
+material.displacementMap = doorHeightTexture
+material.displacementScale = 0.05
+material.metalnessMap = doorMetalnessTexture
+material.roughnessMap = doorRoughnessTexture
+material.normalMap = doorNormalTexture
+material.normalScale.set(0.5, 0.5)
+material.transparent = true
+material.alphaMap = doorAlphaTexture // transparent needed
+*/
