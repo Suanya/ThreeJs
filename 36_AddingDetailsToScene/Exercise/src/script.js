0efe7690d96da497e98db19368bf2a1cdb5f7a27
Suanya <sonjaunger@eyewant.me>
2021-10-15 12:43:56 -0700

    ParticelIN
    Lil' Zwischensafe

--- 36_AddingDetailsToScene/Exercise/src/script.js
+++ 36_AddingDetailsToScene/Exercise/src/script.js
@@ -4,11 +4,16 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import firefliesVertexShader from './shaders/fireflies/vertex.glsl'
+import firefliesFragmentShader from './shaders/fireflies/fragment.glsl'
+
+
 
 /**
  * Base
  */
 // Debug
+const debugObject = {}
 const gui = new dat.GUI({
     width: 400
 })
@@ -75,6 +80,42 @@ const discoMaterial = new THREE.MeshBasicMaterial({ color: 0xFA5BFF })
 )
 
 /**
+ * FireFlies
+ */
+// Geometry
+const firefliesGeometry = new THREE.BufferGeometry()
+const firefliesCount = 30
+const positionArray = new Float32Array(firefliesCount * 3)
+
+for (let i = 0; i < firefliesCount; i++)
+{
+    positionArray[ i * 3 + 0] = (Math.random() - 0.5) * 4
+    positionArray[ i * 3 + 1] = Math.random() * 1.5
+    positionArray[ i * 3 + 2] = (Math.random() - 0.5) * 4
+}
+firefliesGeometry.setAttribute('position', new THREE.BufferAttribute(positionArray, 3))
+console.log(firefliesGeometry)
+
+// Material
+const firefliesMaterial = new THREE.ShaderMaterial({ 
+    uniforms:
+    {
+        uPixelRatio: { value: Math.min(window.devicePixelRatio, 2) },
+        uSize: { value: 100 }
+    },
+    vertexShader: firefliesVertexShader,
+    fragmentShader: firefliesFragmentShader,
+    transparent: true
+})
+
+gui.add(firefliesMaterial.uniforms.uSize, 'value').min(0).max(500).step(1).name('firefliesSize')
+
+// Points
+const fireflies = new THREE.Points(firefliesGeometry, firefliesMaterial)
+scene.add(fireflies)
+
+
+/**
  * Sizes
  */
 const sizes = {
@@ -95,6 +136,9 @@ window.addEventListener('resize', () =>
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+    // Update fireflies
+    firefliesMaterial.uniforms.uPixelRatio.value = Math.min(window.devicePixelRatio, 2)
 })
 
 /**
@@ -122,6 +166,17 @@ renderer.outputEncoding = THREE.sRGBEncoding
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+debugObject.clearColor = '#202121'
+renderer.setClearColor(debugObject.clearColor)
+gui
+    .addColor(debugObject, 'clearColor')
+    .onChange(() =>
+    {
+        renderer.setClearColor(debugObject.clearColor)
+    }
+    )
+
+
 /**
  * Animate
  */
@@ -142,3 +197,12 @@ const tick = () =>
 }
 
 tick()
+
+// Keep it
+/**
+ * FireFlies
+ */
+/*
+// Material
+const firefliesMaterial = new THREE.PointsMaterial({ size: 0.1, sizeAttenuation: true})
+*/
