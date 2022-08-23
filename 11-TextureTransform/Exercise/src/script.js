3b290e0191ba9c02e13c5bc4d31d9797502ccbfd
Sonja <sonjaunger@eyewant.me>
2022-08-23 11:22:49 -0700

    TextureTransform
    Live

--- 11-TextureTransform/Exercise/src/script.js
+++ 11-TextureTransform/Exercise/src/script.js
@@ -2,28 +2,6 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-
-/**
- * Textures
- */
-const loadingManager = new THREE.LoadingManager()
-
-const textureLoader = new THREE.TextureLoader(loadingManager)
-// const colorTexture = textureLoader.load('/textures/checkerboard-1024x1024.png')
-// const colorTexture = textureLoader.load('/textures/checkerboard-8x8.png')
-const colorTexture = textureLoader.load('/textures/minecraft.png')
-// const colorTexture = textureLoader.load('/textures/door/color.jpg')
-const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
-const heightTexture = textureLoader.load('/textures/door/height.jpg')
-const normalTexture = textureLoader.load('/textures/door/normal.jpg')
-const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
-const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
-const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
-
-colorTexture.generateMipmaps = false // better performance
-colorTexture.minFilter = THREE.NearestFilter
-colorTexture.magFilter = THREE.NearestFilter // NearestFilter for better results!
-
 /**
  * Base
  */
@@ -34,10 +12,25 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Textures
+ */
+const loadingManager = new THREE.LoadingManager()
+const textureLoader = new THREE.TextureLoader(loadingManager)
+const colorTexture = textureLoader.load('/textures/door/color.jpg')
+
+colorTexture.minFilter = THREE.NearestFilter
+colorTexture.magFilter = THREE.NearestFilter // NearestFilter for better results!
+
+colorTexture.repeat.x = Math.PI * 4
+colorTexture.repeat.y = Math.PI * 4
+colorTexture.rotation = Math.PI * 0.25
+colorTexture.center.x = 0.5
+colorTexture.center.y = 0.5
+
+/**
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-console.log(geometry.attributes)
 const material = new THREE.MeshBasicMaterial({ map: colorTexture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
@@ -106,58 +99,5 @@ const tick = () =>
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
-
 tick()
 
-
-
-
-
-// Zu schön um wegzuhauen
-
-/**
- * Textures
- */
-
-/* FirstApproach to get this Texture
-const image = new Image()
-const texture = new THREE.Texture(image)
-image.onload = () =>
-{
-    texture.needsUpdate = true
-}
-image.src = '/textures/door/color.jpg'
-*/
-
-/*
-loadingManager.onStart = () =>
-{
-    console.log('onStart')
-}
-loadingManager.onProgress = () =>
-{
-    console.log('onProgress')
-}
-loadingManager.onError = () =>
-{
-    console.log('onError')
-}
-*/
-
-/*
-colorTexture.repeat.x = 2
-colorTexture.repeat.y = 3
-colorTexture.wrapS = THREE.MirroredRepeatWrapping
-colorTexture.wrapT = THREE.MirroredRepeatWrapping
-colorTexture.offset.x = 0.5
-colorTexture.offset.y = 0.5
-colorTexture.rotation = Math.PI * 0.25
-colorTexture.center.x = 0.5
-colorTexture.center.y = 0.5
-*/
-
-/**
- * Object
- */
-// const geometry = new THREE.SphereBufferGeometry(1, 32, 32)
-// const geometry = new THREE.ConeBufferGeometry(1, 1, 32)
