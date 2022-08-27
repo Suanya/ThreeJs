a827cf1d56a67ae658369302ebd49e0269f5bca0
Sonja <sonjaunger@eyewant.me>
2022-08-27 03:47:59 -0700

    check
    check

---
+++ 12_EnvironmentMap/exercise/src/script.js
@@ -0,0 +1,165 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI({ closed: true, width: 400 })
+
+
+/**
+ * Textures
+ */
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+const environmentMapTexture  = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.jpg',
+    '/textures/environmentMaps/0/nx.jpg',
+    '/textures/environmentMaps/0/py.jpg',
+    '/textures/environmentMaps/0/ny.jpg',
+    '/textures/environmentMaps/0/pz.jpg',
+    '/textures/environmentMaps/0/nz.jpg'
+])
+
+/**
+ * Objects
+ */
+// Material
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.7
+material.roughness = 0.2
+material.envMap = environmentMapTexture
+
+gui.add(material, 'metalness').min(0).max(1).step(0.0001)
+gui.add(material, 'roughness').min(0).max(1).step(0.0001)
+gui.add(material, 'aoMapIntensity').min(0).max(10).step(0.0001)
+gui.add(material, 'displacementScale').min(0).max(10).step(0.0001)
+
+// Sphere
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5,64,64), // (,...,16,16)
+    material
+)
+sphere.position.x = -1.5
+sphere.geometry.setAttribute('uv2', new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2))
+
+// Plane
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(1, 1, 100, 100),
+    material
+)
+plane.geometry.setAttribute('uv2', new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2))
+material.side = THREE.DoubleSide
+plane.position.x = 0.5
+
+// Capsule
+const capsule = new THREE.Mesh(
+    new THREE.CapsuleGeometry(0.25, 0.5, 64,128), // *4
+    material
+)
+capsule.position.x = 2.5
+capsule.geometry.setAttribute('uv2', new THREE.BufferAttribute(capsule.geometry.attributes.uv.array, 2))
+
+// Donut
+const donut = new THREE.Mesh(
+    new THREE.TorusGeometry(0.6, 0.075, 64, 128), // ,,16,32
+    material
+)
+donut.position.x = 2.5
+donut.rotation.x = 1
+donut.geometry.setAttribute('uv2', new THREE.BufferAttribute(donut.geometry.attributes.uv.array, 2))
+
+// Add objects to the scene
+scene.add(sphere, plane, capsule, donut)
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(140, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    
+    // Update objects
+    sphere.rotation.x = elapsedTime * 1.6
+    sphere.rotation.y = elapsedTime * 1.6
+    plane.rotation.y = elapsedTime
+    capsule.rotation.y = elapsedTime * 1.6
+    donut.rotation.z = elapsedTime * 2
+    donut.rotation.x = elapsedTime * 2
+    
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+tick()
